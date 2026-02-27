7b7add2559a10b74c92b198ef75881481e50a249
Jeppe <AU780707@uni.au.dk>
2026-02-27 14:17:53 +0100

    Now accepts 1 argument

--- src/main/java/boldify/Main.java
+++ src/main/java/boldify/Main.java
@@ -2,6 +2,18 @@ package boldify;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        allow1Argument(args);
+        String fileToProcess = args[0];
+
+
+        // Succesfull for now
+        System.out.println(fileToProcess);
+    }
+
+    private static void allow1Argument(String[] args){
+        if(args.length!=1){
+            System.err.println("Only 1 argument expected: path to PDF file");
+            System.exit(1);
+        }
     }
 }
